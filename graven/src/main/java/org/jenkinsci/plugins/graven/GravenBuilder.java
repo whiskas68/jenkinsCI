908d3894d390494b90c6a91621c5fe38bcfb1166
whiskas68 <whiskas68@whiskas68.com>
2020-05-28 08:48:50 +0800

    global settings for plugins

---
+++ graven/src/main/java/org/jenkinsci/plugins/graven/GravenBuilder.java
@@ -0,0 +1,78 @@
+package org.jenkinsci.plugins.graven;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.Descriptor;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.IOException;
+
+public class GravenBuilder extends Builder {
+    private final String task;
+
+    private boolean enableCloud;
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        listener.getLogger().println("this is Graven build logger");
+        return true;
+    }
+
+    @DataBoundConstructor
+    public GravenBuilder(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    @Extension
+    public static class GravenDescriptor extends BuildStepDescriptor<Builder>{
+
+        private boolean enableCloud;
+
+        public GravenDescriptor(){
+            load();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            req.bindJSON(this,json.getJSONObject("graven"));
+            save();
+            return true;
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "execute Graven task";
+        }
+
+        public boolean isEnableCloud() {
+            return enableCloud;
+        }
+
+    }
+
+    public boolean isEnableCloud() {
+        return getDescriptor().isEnableCloud();
+    }
+
+    @Override
+    public GravenDescriptor getDescriptor() {
+        return (GravenDescriptor) super.getDescriptor();
+    }
+
+}
